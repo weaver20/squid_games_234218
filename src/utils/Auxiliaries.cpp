8602ee0d994e46b8cdd309f163cbc7fea75cd532
Gilad Levy <l.g@campus.technion.ac.il>
2021-12-05 08:38:11 +0200

    making some changes in types and adding task funcs

--- src/utils/Auxiliaries.cpp
+++ src/utils/Auxiliaries.cpp
@@ -1,24 +1,24 @@
 #include "Auxiliaries.h"
 
-Group::Player::Player(uint32_t id, std::shared_ptr<Group> group, uint32_t level): _id(id), _group(group), _level(level) {}
+Player::Player(uint32_t id, std::shared_ptr<Group> group, uint32_t level): _id(id), _group(group), _level(level) {}
 
-uint32_t Group::Player::getID() const {
+uint32_t Player::getID() const {
     return _id;
 }
 
-void Group::Player::setID(uint32_t id) {
+void Player::setID(uint32_t id) {
     _id = id;
 }
 
-uint32_t Group::Player::getLevel() const{
+uint32_t Player::getLevel() const{
     return _level;
 }
 
-void Group::Player::setLevel(uint32_t level) {
+void Player::setLevel(uint32_t level) {
     _level = level;
 }
 
-std::shared_ptr<Group>& Group::Player::getGroup(){
+std::shared_ptr<Group>& Player::getGroup(){
     return _group;
 }
 
@@ -36,13 +36,13 @@ uint32_t Group::getNumOfPlayers() const {
     return _num_of_players;
 }
 
-AVL_Tree<Player_Key, std::shared_ptr<Group::Player>> Group::getGroupPlayers() {
+AVL_Tree<Player_Key, std::shared_ptr<Player>>& Group::getGroupPlayers() {
     return _group_players;
 }
 
 Player_Key::Player_Key(uint32_t id, uint32_t level): _id(id),  _level(level){}
 
-bool Player_Key::operator<(Player_Key &other) const {
+bool Player_Key::operator<(const Player_Key &other) const {
     if (_level < other._level) {
         return true;
     }
@@ -54,11 +54,11 @@ bool Player_Key::operator<(Player_Key &other) const {
     return false;
 }
 
-bool Player_Key::operator==(Player_Key &other) {
+bool Player_Key::operator==(const Player_Key &other) const {
     return not((*this < other) or (*this > other));
 }
 
-bool Player_Key::operator>(Player_Key &other) {
+bool Player_Key::operator>(const Player_Key &other) const {
     return not((*this < other) or (*this == other));
 }
 
@@ -69,5 +69,4 @@ uint32_t Player_Key::getId() const {
 uint32_t Player_Key::getLevel() const {
     return _level;
 }
-
 
--- src/utils/Auxiliaries.h
+++ src/utils/Auxiliaries.h
@@ -5,6 +5,8 @@
 #include "../Tree/Tree.hpp"
 #include "../../lib/library1.h"
 
+class Player;
+
 class Player_Key{
 private:
     uint32_t _id;
@@ -13,45 +15,21 @@ private:
 public:
     Player_Key(uint32_t id, uint32_t level);
     ~Player_Key() = default;
-    Player_Key(const Player_Key& other) = delete;
-    Player_Key& operator=(const Player_Key& other) = delete;
+    Player_Key(const Player_Key& other) = default;
+    Player_Key& operator=(const Player_Key& other) = default;
     uint32_t getId() const;
     uint32_t getLevel() const;
 
-    bool operator<(Player_Key& other) const;
-    bool operator==(Player_Key& other);
-    bool operator>(Player_Key& other);
+    bool operator<(const Player_Key& other) const;
+    bool operator==(const Player_Key& other) const;
+    bool operator>(const Player_Key& other) const;
 };
 
 class Group{
 private:
     uint32_t _id;
     uint32_t _num_of_players;
-
-    class Player{
-    private:
-        uint32_t _id;
-        std::shared_ptr<Group> _group;
-        uint32_t _level;
-        friend class Group;
-
-    public:
-        /****** Default class functions ******/
-        Player(uint32_t id, std::shared_ptr<Group> group, uint32_t level = 0);
-        ~Player() = default;
-        Player& operator=(const Player& other) = default;
-        Player(const Player& other) = default;
-
-        /****** Type functions ******/
-        uint32_t getID() const;
-        void setID(uint32_t id);
-
-        uint32_t getLevel() const;
-        void setLevel(uint32_t level);
-
-        std::shared_ptr<Group>& getGroup();
-    };
-    AVL_Tree<Player_Key, std::shared_ptr<Group::Player>> _group_players;
+    AVL_Tree<Player_Key, std::shared_ptr<Player>> _group_players;
 public:
 
     /****** Default class functions ******/
@@ -66,14 +44,40 @@ public:
 
     uint32_t getNumOfPlayers() const;
 
-    AVL_Tree< Player_Key, std::shared_ptr<Group::Player>> getGroupPlayers();
+    AVL_Tree< Player_Key, std::shared_ptr<Player>>& getGroupPlayers();
+};
+
+class Player{
+private:
+    uint32_t _id;
+    std::shared_ptr<Group> _group;
+    uint32_t _level;
+    friend class Group;
+
+public:
+    /****** Default class functions ******/
+    Player(uint32_t id, std::shared_ptr<Group> group, uint32_t level = 0);
+    ~Player() = default;
+    Player& operator=(const Player& other) = default;
+    Player(const Player& other) = default;
+
+    /****** Type functions ******/
+    uint32_t getID() const;
+    void setID(uint32_t id);
+
+    uint32_t getLevel() const;
+    void setLevel(uint32_t level);
+
+    std::shared_ptr<Group>& getGroup();
 };
 
 /*************** definitions of typedef ***************/
 
 typedef AVL_Tree<uint32_t , std::shared_ptr<Group>> group_tree;
-typedef AVL_Tree<class Player_Key, std::shared_ptr<class Player>> players_tree;
+typedef AVL_Tree<Player_Key, std::shared_ptr<Player>> players_tree;
+typedef AVL_Tree<uint32_t , std::shared_ptr<class Player>> players_by_id_tree;
 typedef std::shared_ptr<class Player> player;
 typedef std::shared_ptr<Group> group;
+
 
 #endif //SQUID_GAMES_234218_AUXILIARIES_H
